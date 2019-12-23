7ac90d9e3f07a025e938504253336f4af82f5565
Abiral Rai <abiralrai182@gmail.com>
2019-12-23 22:20:53 +1100

    Added Ant Design UI library
    Ant Design to speed UI development

---
+++ client/src/components/SideNav/SideNav.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Layout } from "antd";
+
+const { Sider } = Layout;
+
+const SideNav = () => {
+  return (
+    <Sider className="p-32">
+      <h1>Testing</h1>
+    </Sider>
+  );
+};
+
+export default SideNav;
---
+++ client/src/components/TopNav/TopNav.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Layout } from "antd";
+
+const { Header } = Layout;
+
+const TopNav: React.FC = () => {
+  return (
+    <Header>
+      <h1> TOP NAV</h1>
+    </Header>
+  );
+};
+
+export default TopNav;
