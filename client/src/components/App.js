83a984eb5c5e07a9f525048244c8afccefe9c588
Abiral Rai <abiralrai182@gmail.com>
2019-12-24 11:59:15 +1100

    Removed Typescript
    Revert back to just React.
    Created base layout for dashboard --incomplete

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'antd';
+import Dashboard from './Dashboard'
+import '../assets/styles/app.css'
+
+function App() {
+  return (
+    <div className="bg-gray-100">
+    <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
