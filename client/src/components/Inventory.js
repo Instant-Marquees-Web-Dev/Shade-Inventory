2b53819be03bfdb05b7270d8149928f29efe87e6
Abiral Rai <abiralrai182@gmail.com>
2020-03-09 21:15:02 +1100

    Updates inventory from Modal - Read More
    Working fine but does not update in form modal

--- client/src/components/Inventory.js
+++ client/src/components/Inventory.js
@@ -1,6 +1,66 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {Modal, Form, Input} from 'antd'
+import inventoryData from '../utils/inventoryData'
 
 const Inventory = () => {
+  const [inventories, setInventories] = useState([])
+  const [currentIndex, setCurrentIndex] = useState(null)
+  const [currentItem, setCurrentItem] = useState({})
+  const [modalVisible, setModalVisible] = useState(false)
+  const [form] = Form.useForm()
+
+  const itemName = currentItem.name || ''
+  const itemQuantity = currentItem.quantity || null
+
+  useEffect(() => {
+    setInventories(inventoryData) // Update inventory state, NOTE: This data will be grabbed from database 
+
+  }, [])
+
+
+  const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+  
+    const handleEdit = item => {
+      editInventory(item)
+    }
+
+  const editInventory = item => {
+    showModal()
+    setCurrentIndex(item)
+    setCurrentItem(inventories[item])
+  }
+
+  const showModal = () => {
+    setModalVisible(true)
+    
+  }
+
+  const handleOk = e => {
+    form
+    .validateFields()
+    .then(({name, quantity}) => {
+      form.resetFields()
+      const newName = name ? name : currentItem.name
+      const newQuantity = quantity ? quantity : currentItem.quantity
+      let newInventory = [...inventories]
+      newInventory[currentIndex] = {name:newName, quantity: newQuantity}
+      setInventories(newInventory)
+
+    })
+    .catch(info => console.log('Validate Failed', info))
+    setModalVisible(false)
+  }
+  const handleCancel = e => {
+    setModalVisible(false)
+  }
+
   return (
     <div className="flex justify-center">
       <div className="-my-2 py-2 overflow-x-auto w-11/12 sm:-mx-6 sm:px-6 sm:w-9/12 lg:-mx-8 lg:px-8 lg:w-9/12 xl:w-1/3 ">
@@ -19,31 +79,50 @@ const Inventory = () => {
               </tr>
             </thead>
             <tbody className="bg-white">
-              <tr>
+              {inventories.map(({name, quantity}, i) => (
+                <tr key={i}>
                 <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 font-medium text-gray-900">
-                  Legs
+                  {name}
                 </td>
                 <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-900">
-                  30
+                  {quantity}
                 </td>
                 <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                  <a href="#" className="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline">Edit</a>
+                  <a href="#" className="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline" onClick={()=> handleEdit(i)}>Edit</a>
                 </td>
               </tr>
-              <tr>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 font-medium text-gray-900">
-                  Legs
-                </td>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-900">
-                  30
-                </td>
-                <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                  <a href="#" className="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline">Edit</a>
-                </td>
-              </tr>
+              ))}
             </tbody>
           </table>
         </div>
+        <Modal
+          title="Edit Inventory"
+          visible={modalVisible}
+          onOk={handleOk}
+          onCancel={handleCancel}>
+            <Form
+              {...layout}
+              name="Edit Inventory"
+              form={form}
+              >
+              <Form.Item
+                label="Name"
+                name="name"
+              >
+              <Input defaultValue={itemName} />
+              </Form.Item>
+
+              <Form.Item
+                label="Quantity"
+                name="quantity"
+              >
+              <Input 
+              defaultValue={itemQuantity}
+
+              />
+              </Form.Item>
+            </Form>
+        </Modal>
       </div>
     </div>  
   )
---
+++ client/src/utils/inventoryData.js
@@ -0,0 +1,82 @@
+
+const inventoryData = [
+  {
+    name: "Bananas",
+    quantity: 30
+  },
+  {
+    name: "Gable Beam",
+    quantity: 20
+  },
+  {
+    name: "Gable Upright",
+    quantity: 30
+  },
+  {
+    name: "Intermediate",
+    quantity: 30
+  },
+  {
+    name: "Eave Purlin",
+    quantity: 30
+  },
+  {
+    name: "Roof Beam",
+    quantity: 30
+  },
+  {
+    name: "Elbow Connector",
+    quantity: 30
+  },
+  {
+    name: "Cross Brace",
+    quantity: 30
+  },
+  {
+    name: "Legs",
+    quantity: 30
+  },
+  {
+    name: "Feet/Cage",
+    quantity: 30
+  },
+  {
+    name: "Wall Bar",
+    quantity: 30
+  },
+  {
+    name: "Corner Roof",
+    quantity: 30
+  },
+  {
+    name: "St Roof ",
+    quantity: 30
+  },
+  {
+    name: "Pegs (only if pegged)",
+    quantity: 30
+  },
+  {
+    name: "Ratchets",
+    quantity: 30
+  },
+  {
+    name: "Arc",
+    quantity: 30
+  },
+  {
+    name: "Roof",
+    quantity: 30
+  },
+  {
+    name: "Gables",
+    quantity: 30
+  },
+  {
+    name: "Walls",
+    quantity: 30
+  },
+  
+]
+
+export default inventoryData;
