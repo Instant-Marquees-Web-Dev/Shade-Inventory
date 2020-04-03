a376e2d75ecb6d14cf7d5467488df34b829b83a6
Abiral Rai <abiralrai182@gmail.com>
2020-04-03 17:50:10 +1100

    Styling issue

--- client/src/elements/InventoryTable.js
+++ client/src/elements/InventoryTable.js
@@ -3,7 +3,7 @@ import React from 'react'
 const InventoryTable = ({inventories, handleEdit}) => {
   return (
     <div className="flex justify-center">
-      <div className="-my-2 py-2 overflow-x-auto w-11/12 sm:-mx-6 sm:px-6 sm:w-9/12 lg:-mx-8 lg:px-8 lg:w-9/12 xl:w-1/3 ">
+      <div className="-my-2 py-2 overflow-x-auto w-11/12 sm:-mx-6 sm:px-6 sm:w-9/12 lg:-mx-8 lg:px-8 lg:w-9/12 xl:w-2/3 ">
       <h1 className="text-gray-900 font-bold text-xl mb-2 ">Inventory</h1>
         <div className="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
           <table className="min-w-full">
@@ -28,7 +28,7 @@ const InventoryTable = ({inventories, handleEdit}) => {
                   {quantity}
                 </td>
                 <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                  <btn  className="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline" onClick={()=> handleEdit(i)}>Edit</btn>
+                  <button className="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline" onClick={()=> handleEdit(i)}>Edit</button>
                 </td>
               </tr>
               ))}
