4f5db36bb074e74211bf61fbf41eb3fe6505f346
Abiral Rai <abiralrai182@gmail.com>
2020-04-12 14:04:18 +1000

    A Bit better Weather Component

--- client/src/elements/WeatherWidget.js
+++ client/src/elements/WeatherWidget.js
@@ -5,13 +5,26 @@ import dayjs from 'dayjs'
 
 const { TabPane } = Tabs 
 
+const computeNearestHr = () => {
+  const currentTimeInHrs = new Date().getHours();
+  const constHrs = [0, 3, 6, 9, 12, 15, 18, 21];
+  const differences = constHrs.map(e => Math.abs(e - currentTimeInHrs));
+  const indexofLowestDiff = differences.indexOf(Math.min(...differences));
+
+  return constHrs[indexofLowestDiff]
+}
+
+const SET_CURRENT_DATE = () => {
+    return dayjs().format('YYYY/MM/DD').concat(`, ${computeNearestHr()}`).toString()
+}
+
 const WeatherWidget = () => {
   const [weather, setWeather] = useState({
     error: false,
     loading: true,
     forecast:{}
   })
-  const [currentDay, setDay] = useState('')
+  const [currentDay, setCurrentDay] = useState(SET_CURRENT_DATE)
   const USER_INPUT= 'springvale'
   useEffect(() => {
     fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${USER_INPUT},Victoria,AU&appid=${process.env.REACT_APP_WEATHER_API}&units=metric`)
@@ -22,7 +35,6 @@ const WeatherWidget = () => {
           loading: false,
           forecast: data
         })
-        // setDay(weather.forecast.list[0].dt_txt)
       })
       .catch(e => setWeather({
         error: true,
@@ -51,38 +63,37 @@ const WeatherWidget = () => {
     list // lists of 40 readings 5day / 3 hours
   }= forecast
 
-
+  
   const currentData = () => {
-    console.log(list.find(e => new Date(e.dt_txt).getHours() === computeNearestHr()))
+    return (list.find(e => new Date(e.dt_txt).getHours() === computeNearestHr()))
   }
 
-  const computeNearestHr = () => {
-    const currentTimeInHrs = new Date().getHours();
-    const constHrs = [0, 3, 6, 9, 12, 15, 18, 21];
-    const differences = constHrs.map(e => Math.abs(e - currentTimeInHrs));
-    const indexofLowestDiff = differences.indexOf(Math.min(...differences));
-
-    return constHrs[indexofLowestDiff]
-  }
   return (
     <div className="rounded overflow-hidden shadow-lg mt-16 bg-white px-4 py-5">
     <h3 className="text-xl leading-4 font-medium text-gray-900">
             {name} VIC
           </h3>
-
       <Tabs
         defaultActiveKey={currentDay}
         tabPosition="bottom"
-        onTabClick={day => setDay(day)}
       >
       {
         list.map(({dt, main:{temp, humidity}, weather:[{description, icon}], wind:{speed}, rain, dt_txt}) => (
           <TabPane
-            tab={dayjs(dt_txt).format('ddd')}
-            key={dt_txt} 
+            key={dayjs(dt_txt).format('YYYY/MM/DD, H')} 
+            tab={
+              <div className="flex flex-col">
+                <span>
+                  {dayjs(dt_txt).format('ddd')}
+                </span>
+                <span>
+                  {dayjs(dt_txt).format('h a')}
+                </span>
+              </div>
+              }
           >
             <p className="-mt-1 text-sm leading-5 text-gray-500">
-              {dayjs(currentDay).format('dddd, h:mm a')}
+              {dayjs(dt_txt).format('dddd, h:mm a')}
             </p>
             <p className="-mt-4 text-sm leading-5 text-gray-500">
               {description}
